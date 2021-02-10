015b1b8a0e075e7e0e1b06eeffaba0d146d0956e
Qiyuan Gong <qiyuan.gong@intel.com>
2021-02-10 19:51:36 +0800

    Refine model link

--- demos/openvino/download_openvino_model.sh
+++ demos/openvino/download_openvino_model.sh
@@ -3,5 +3,5 @@ set -e
 
 rm -rf model && mkdir model
 cd model
-wget https://download.01.org/opencv/2019/open_model_zoo/R3/20190905_163000_models_bin/age-gender-recognition-retail-0013/FP32/age-gender-recognition-retail-0013.bin
-wget https://download.01.org/opencv/2019/open_model_zoo/R3/20190905_163000_models_bin/age-gender-recognition-retail-0013/FP32/age-gender-recognition-retail-0013.xml
+wget https://download.01.org/opencv/2020/openvinotoolkit/2020.2/open_model_zoo/models_bin/2/age-gender-recognition-retail-0013/FP32/age-gender-recognition-retail-0013.bin
+wget https://download.01.org/opencv/2020/openvinotoolkit/2020.2/open_model_zoo/models_bin/2/age-gender-recognition-retail-0013/FP32/age-gender-recognition-retail-0013.xml
